8016bef76ebcd86b8ae4a47c5f4eb1c946899d94
Ksenia D <Ksenia D>
2017-03-27 21:26:44 -0400

    saving new burgers in data base, moving burgers into devoured list, updating database

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
 
+
+var exphbs = require("express-handlebars");
+
+// Sets up the Express App
+// =============================================================
 var app = express();
-var port = 3000;
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "/public"));
-
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+var PORT = process.env.PORT || 3000;
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 
-var exphbs = require("express-handlebars");
-
+// Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + "/public"));
+var connection = require("./config/connection.js");
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+var routes = require("./controllers/burgers_controller.js");
 
-  console.log("connected as id " + connection.threadId);
+app.use("/", routes);
+app.use("/:id", routes);
+app.use("/create", routes);
 
-});
+
+
+app.listen(PORT);
